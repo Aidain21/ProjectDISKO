3bed331508d8a14dfdb79b056f4f4488803c8f15
Aidan Ainsley <aidanainsley@gmail.com>
2025-08-31 20:36:36 -0400

    Added Music and Rhythm based inputs
    Fixed floaty fall, added cam for back area (bit buggy), combo tracker

---
+++ Assets/MusicScript.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Collections;
+
+public class MusicScript : MonoBehaviour
+{
+    public AudioSource music;
+    public AudioClip[] tracks;
+    public bool onTempo;
+    public int bpm;
+    public float timer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().enabled = false;
+        music = GetComponent<AudioSource>();
+        ChangeMusic(0, 70);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= 60f/bpm)
+        {
+            StartCoroutine(OnBeat());
+            timer = 0;
+        }
+    }
+
+    public IEnumerator OnBeat()
+    {
+        GetComponent<SpriteRenderer>().enabled = true;
+        onTempo = true;
+        yield return new WaitForSeconds(0.2f);
+        GetComponent<SpriteRenderer>().enabled = false;
+        onTempo = false;
+        yield return new WaitForSeconds(0.1f);
+        transform.parent.GetComponent<PlayerScript>().alreadyBoosted = false;
+    }
+
+    public void ChangeMusic(int track, int _bpm)
+    {
+        music.clip = tracks[track];
+        music.Play();
+        bpm = _bpm;
+    }
+}
+
+
--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -5,10 +5,11 @@ public class PlayerScript : MonoBehaviour
     Rigidbody rb;
     public Camera cam;
     public float baseSpeed = 3f, curSpeed = 3f, maxSpeed = 10f, jumpForce;
-    public bool canJump = true;
-    public TMP_Text speedTracker;
+    public bool canJump = true, alreadyBoosted;
+    public TMP_Text speedTracker, ballerText, comboText;
     public bool baller;
-    public TMP_Text ballerText;
+    public int invert, combo;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,11 +20,26 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        speedTracker.text = (Mathf.Round(rb.linearVelocity.x * 100)/100) + " " + (Mathf.Round(rb.linearVelocity.y * 100) / 100) + " " + (Mathf.Round(rb.linearVelocity.z * 100) / 100);
+        speedTracker.text = (Mathf.Round(rb.linearVelocity.x * 100)/100).ToString("F2") + 
+            " " + (Mathf.Round(rb.linearVelocity.y * 100) / 100).ToString("F2") + 
+            " " + (Mathf.Round(rb.linearVelocity.z * 100) / 100).ToString("F2");
+
         //Moves camera with player
-        cam.transform.position = new Vector3(transform.position.x, transform.position.y + 2.5f, cam.transform.position.z);
+        if (transform.position.z <= 2.5f)
+        {
+            invert = 1;
+            cam.transform.position = new Vector3(transform.position.x, transform.position.y + 2.5f, -5);
+            cam.transform.eulerAngles = new Vector3(25, 0, 0);
+        }
+        else if (transform.position.z >= 3.5f)
+        {
+            invert = -1;
+            cam.transform.position = new Vector3(transform.position.x, transform.position.y + 2.5f, 10);
+            cam.transform.eulerAngles = new Vector3(25, 180, 0);
+        }
+        
         //Moves player
-        rb.linearVelocity = new Vector3(Input.GetAxisRaw("Horizontal") *curSpeed, rb.linearVelocity.y, Input.GetAxisRaw("Vertical") * curSpeed);
+        rb.linearVelocity = new Vector3(Input.GetAxisRaw("Horizontal") *curSpeed * invert, rb.linearVelocity.y, Input.GetAxisRaw("Vertical") * curSpeed * invert);
         //Speed up over time
         if (rb.linearVelocity != Vector3.zero)
         {
@@ -40,23 +56,34 @@ public class PlayerScript : MonoBehaviour
         {
             curSpeed = baseSpeed;   
         }
-
+        //jump
         if (Input.GetKeyDown(KeyCode.Space) && canJump)
         {
             rb.AddForce(Vector3.up * 7, ForceMode.Impulse);
-            canJump = false;
+            if (transform.GetChild(0).GetComponent<MusicScript>().onTempo && !alreadyBoosted)
+            {
+                ComboUp(combo);
+                alreadyBoosted = true;
+                rb.AddForce(Vector3.up * 3, ForceMode.Impulse);
+            }
+            else
+            {
+                ComboUp(-1);
+                canJump = false;
+            }
+            
         }
-
-        if (Input.GetKeyUp(KeyCode.Space) && rb.linearVelocity != Vector3.zero)
+        //allows for small jumps
+        if (Input.GetKeyUp(KeyCode.Space) && rb.linearVelocity != Vector3.zero && rb.linearVelocity.y > 0)
         {
             rb.linearVelocity = new Vector3(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f, rb.linearVelocity.z);
         }
-
+        //repsawn
         if (transform.position.y < -25)
         {
             transform.position = Vector3.up;
         }
-
+        //B A L L E R
         if (Input.GetKeyDown(KeyCode.B))
         {
             if (baller)
@@ -73,7 +100,14 @@ public class PlayerScript : MonoBehaviour
                 ballerText.text = "Press B to become B A L L \nBall Status: It's Roll Time";
             }
         }
-        
+
+    }
+
+    void ComboUp(int com)
+    {
+        combo = com + 1;
+        string wow = new('!', combo);
+        comboText.text = "Combo: " + combo + wow;
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -82,5 +116,9 @@ public class PlayerScript : MonoBehaviour
         {
             canJump = true;
         }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
     }
 }
